2395c0e3f8b8e8a2255a4bb20382f93414c61ea7
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-05-16 22:56:58 +0200

    aggiunta interattivita cella quando numero random=numero nella cella

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,13 +3,16 @@
 //---------------------------PATRE STATICA-----------------
 //1. devo creare il numero di celle contenente i numei da inserire
 //2. devo inserirle, appenderle nel file html
+
 const array = [];
-const createCells = (cells) => {
+const arrayRandomNumbers = [];
+
+const createCells = (range) => {
   //1. cerco riferimento al div 'tabellone'
   const tabellone = document.getElementById("tabellone");
 
   //2. creo adesso le celle da inserire dentro il tabellone (76 celle da 1 a 76)
-  for (let index = 0; index < cells; index++) {
+  for (let index = 0; index < range; index++) {
     //2.1 ad ogni ciclo creo un div,ovvero la cella che conterrà il numero
     const tombolaCell = document.createElement("div");
 
@@ -27,31 +30,30 @@ const createCells = (cells) => {
 
     //2.5 appendo la cella al tabellone
     tabellone.appendChild(tombolaCell);
-
-    //2.6. creo riferimento al bottone della pagina
-    const button = document.querySelector("button");
-
-    //2.7. aggiungo funzione al click del pulsante per generare il numero e restituirlo dentro ad un h2
-    button.addEventListener("click", () => {
-      const extractNumber = generateRandomNumber();
-
-      //2.8.riporto il numero estratto su un h2
-      const numberDisplayed = document.querySelector("h2");
-      numberDisplayed.innerText = extractNumber;
-
-      if (generateRandomNumber() === array[index])
-        cellNumber.classList.add("selected");
-    });
-  }
+  } //chiudo il FOR
   console.log(array);
 };
 
 //3. creo funzione per generare un numero random da 1 a 76
-const generateRandomNumber = () => {
+const getRandomNumber = () => {
   const randomNumber = Math.floor(Math.random() * 76) + 1;
   return randomNumber;
 };
 
-const cells = 76;
-createCells(cells);
-generateRandomNumber();
+const generateRandomNumber = (range) => {
+  const random = getRandomNumber(range);
+  //2.8.riporto il numero estratto su un h2
+  const numberDisplayed = document.querySelector("h2");
+  numberDisplayed.innerText = random;
+  const cells = document.querySelectorAll(".cell");
+  cells[random - 1].classList.add("selected");
+};
+
+window.onload = () => {
+  const range = 76;
+  createCells(range);
+  const randomButton = document.getElementById("random-bttn");
+  randomButton.addEventListener("click", () => {
+    generateRandomNumber(range);
+  });
+};
